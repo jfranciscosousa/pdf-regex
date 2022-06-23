5ee8dd5608af392d44c38c038b21671185c1f556
Francisco Sousa <francisco.sousa@hey.com>
2022-06-23 10:54:57 +0100

    Fixes

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,12 @@
 <!-- @hmr:keep-all -->
 <script lang="ts">
   import Input from "./components/Input.svelte";
-  import { loadPDFJS } from "./lib/pdfutils";
   import pdfStore from "./stores/pdfStore";
 
   let url: string;
-  let pdfContainer: HTMLDivElement;
+
   let regexString: string;
   let regex = new RegExp("", "g");
-
-  $: {
-    if ($pdfStore.document && pdfContainer) {
-      loadPDFJS($pdfStore.document, pdfContainer);
-    }
-  }
 
   $: {
     try {
@@ -26,8 +19,12 @@
 
 {#if $pdfStore.document}
   <div class="parent h-screen">
-    <div class="div1 border-black border-2">
-      <div class="h-full w-full" bind:this={pdfContainer} />
+    <div class="div1 border-black border-2 relative">
+      <object data={url} type="application/pdf" class="w-full h-full">
+        <iframe src={url} class="w-full h-full">
+          <p>This browser does not support PDF!</p>
+        </iframe>
+      </object>
     </div>
     <div class="div2 border-black border-2">
       {@html $pdfStore.text.replace(
@@ -48,7 +45,7 @@
   <Input bind:url />
 {/if}
 
-<style>
+<style lang="postcss">
   .parent {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
@@ -60,10 +57,22 @@
   .div1 {
     grid-area: 1 / 1 / 2 / 2;
     overflow: auto;
+
+    @media screen(md) {
+      display: none;
+    }
+
+    * :global(canvas) {
+      margin: 0 auto;
+    }
   }
   .div2 {
     grid-area: 1 / 2 / 2 / 3;
     overflow: auto;
+
+    @media screen(md) {
+      grid-area: 1 / 1 / 2 / 3;
+    }
   }
   .div3 {
     grid-area: 2 / 1 / 3 / 3;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,svelte,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,svelte,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      "2xl": { max: "1535px" },
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      md: { max: "767px" },
+      sm: { max: "639px" },
+    },
   },
   plugins: [],
-}
+};
